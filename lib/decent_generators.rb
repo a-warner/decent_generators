c17e7f2dd1d1cf4d3d5448104cd864217426ec04
Andrew Warner <wwarner.andrew@gmail.com>
2013-10-02 09:48:31 -0400

    Kill useless before_initialize block

--- lib/decent_generators.rb
+++ lib/decent_generators.rb
@@ -3,8 +3,5 @@ require 'rails'
 module DecentGenerators
   class Railtie < ::Rails::Railtie
     config.app_generators.templates << File.expand_path('../templates', __FILE__)
-
-    config.before_initialize do
-    end
   end
 end
